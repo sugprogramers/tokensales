29479089fbc709307848e309fab9c39325b567d5
user <user@aceerp01>
2018-08-04 20:13:42 -0500

    fix add_user seed

--- db/seeds/2_add_users.sql
+++ db/seeds/2_add_users.sql
@@ -1,9 +1,9 @@
 INSERT INTO c_user(
             c_user_id, isactive, created, createdby, updated, updatedby, 
-            username, password, phone, firstname, lastname, birthday, address1, 
+            password, phone, firstname, lastname, birthday, address1, 
             address2, c_country_id, c_region_id, city, postal, usertype, 
             email, registertoken, tokenexpirationdate, status)
     VALUES ('100', 'Y', NOW(), '100', NOW(), '100', 
-            'admin', 'admin', '', '', '', NOW(), '', 
+            'admin', '', '', '', NOW(), '', 
             '', '100', '103', '', '', 'ADM', 
             'admin@gmail.com', '', NOW(), '');
