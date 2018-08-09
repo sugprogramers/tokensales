29a8d571857236d28ef551301d17aabf93e67bef
Oscar <>
2018-08-09 10:04:54 -0500

    projects 1.2

--- application/views/company_list_project.php
+++ application/views/company_list_project.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="<?php echo base_url() . "themes/default/remark/topbar"; ?>/assets/examples/css/apps/media.min599c.css?v4.0.2">
-        
+
 <div class="page">
     <div class="page-header">
         <h1 class="page-title">My Projects</h1>
@@ -15,73 +15,70 @@
         <div class="panel">
             <div class="panel-body">
 
+
+
+
                 <div class="app-media" > 
 
                     <div class="page-main">
-                        <div  class="page-content page-content-table" data-plugin="selectable">
+                        <div  class="page-content page-content-table" >
 
                             <!-- Actions -->
-                            <div class="page-content-actions">
+                            <div class="page-content-actions" style="border-bottom: 1px solid #e4eaec;padding: 0 0 5px;">
                                 <div class="float-right">
                                     <div class="btn-group media-arrangement" role="group">
                                         <button class="btn btn-outline btn-default active" id="arrangement-grid" type="button"><i class="icon wb-grid-4" aria-hidden="true"></i></button>
                                         <button class="btn btn-outline btn-default" id="arrangement-list" type="button"><i class="icon wb-list" aria-hidden="true"></i></button>
                                     </div>
                                 </div>
-                               <!-- <div class="actions-inner">
-                                    <div class="checkbox-custom checkbox-primary checkbox-lg">
-                                        <input type="checkbox" id="media_all" class="selectable-all">
-                                        <label for="media_all"></label>
-                                    </div> 
-                                </div> -->
+
+                                <div class="float-left">
+                                    <div class="form-group ">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" name="" placeholder="Search...">
+                                            <span class="input-group-btn">
+                                                <button type="submit" class="btn btn-primary"><i class="icon wb-search" aria-hidden="true"></i></button>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
 
-                            <!-- Media -->
+
                             <div class="media-list is-grid pb-50" data-plugin="animateList" data-animate="fade"
                                  data-child="li">
                                 <ul class="blocks blocks-100 blocks-xxl-4 blocks-xl-3 blocks-lg-3 blocks-md-2 blocks-sm-2"
                                     data-plugin="animateList" data-child=">li">
                                     <li>
-                                        <div class="media-item" data-toggle="slidePanel" data-url="panel.tpl">
-                                            <!--<div class="checkbox-custom checkbox-primary checkbox-lg">
-                                                <input type="checkbox" class="selectable-item" id="media_1" />
-                                                <label for="media_1"></label>
-                                            </div> -->
-                                            <div class="image-wrap">
-                                               <!-- <img class="image img-rounded" src="<?php echo base_url() . "themes/default/remark"; ?>/global/photos/view-1-960x640.jpg"
-                                                     alt="..."> -->
-                                                
+                                        <div class="media-item" >
+
+                                            <div class="image-wrap"  data-toggle="slidePanel" data-url="<?php echo base_url() . "themes/default/tpl"; ?>/panel.tpl">
                                                 <figure class="overlay overlay-hover">
-                    <img class="overlay-figure" src="<?php echo base_url() . "themes/default/remark"; ?>/global/photos/focus-5-960x640.jpg" alt="...">
-                    <figcaption class="overlay-panel overlay-background overlay-slide-left">
-                      <h3>Title</h3>
-                      <p>Lorem <a href="javascript:void(0)">ipsum dolor</a> sit amet,
-                        consetetur sadipscing elitr.</p>
-                    </figcaption>
-                  </figure>
+                                                    <img class="overlay-figure" src="<?php echo base_url() . "themes/default/remark"; ?>/global/photos/focus-5-960x640.jpg" alt="...">
+                                                    <figcaption class="overlay-panel overlay-background overlay-slide-left">
+                                                        <div class="img-text-hover"><h3>Title</h3>
+                                                            <p>Lorem <a href="javascript:void(0)">ipsum dolor</a> sit amet,consetetur sadipscing elitr.</p>
+                                                        </div>
+                                                    </figcaption>
+                                                </figure>
                                             </div>
                                             <div class="info-wrap">
                                                 <div class="dropdown">
                                                     <span class="icon wb-settings" data-toggle="dropdown" aria-expanded="false" role="button"
-                                                          data-animation="scale-up" ></span>
+                                                          data-animations="fadeInDown fadeInLeft fadeInUp fadeInRight"  ></span>
                                                     <div class="dropdown-menu dropdown-menu-right" role="menu">
                                                         <a class="dropdown-item" href="javascript:void(0)"><i class="icon wb-pencil" aria-hidden="true"></i>Edit</a>
-                                                        <a class="dropdown-item" href="javascript:void(0)"><i class="icon wb-download" aria-hidden="true"></i>Download</a>
                                                         <a class="dropdown-item" href="javascript:void(0)"><i class="icon wb-trash" aria-hidden="true"></i>Delete</a>
                                                     </div>
-                                                </div>
+                                                </div> 
                                                 <div class="title">Lorem ipsum</div>
                                                 <div class="time">1 minutes ago</div>
-                                                <div class="media-item-actions btn-group">
+                                                <div class="media-item-actions btn-group" >
                                                     <button class="btn btn-icon btn-pure btn-default" data-original-title="Edit" data-toggle="tooltip"
                                                             data-container="body" data-placement="top" data-trigger="hover"
                                                             type="button">
                                                         <i class="icon wb-pencil" aria-hidden="true"></i>
-                                                    </button>
-                                                    <button class="btn btn-icon btn-pure btn-default" data-original-title="Download"
-                                                            data-toggle="tooltip" data-container="body" data-placement="top"
-                                                            data-trigger="hover" type="button">
-                                                        <i class="icon wb-download" aria-hidden="true"></i>
                                                     </button>
                                                     <button class="btn btn-icon btn-pure btn-default" data-original-title="Delete"
                                                             data-toggle="tooltip" data-container="body" data-placement="top"
@@ -93,6 +90,85 @@
                                         </div>
                                     </li>
 
+                                    <li>
+                                        <div class="media-item" >
+
+                                            <div class="image-wrap"  data-toggle="slidePanel" data-url="<?php echo base_url() . "themes/default/tpl"; ?>/panel.tpl">
+                                                <figure class="overlay overlay-hover">
+                                                    <img class="overlay-figure" src="<?php echo base_url() . "themes/default/remark"; ?>/global/photos/focus-6-960x640.jpg" alt="...">
+                                                    <figcaption class="overlay-panel overlay-background overlay-slide-top">
+                                                        <div class="img-text-hover"><h3>Title</h3>
+                                                            <p>Lorem <a href="javascript:void(0)">ipsum dolor</a> sit amet,consetetur sadipscing elitr.</p>
+                                                        </div>
+                                                    </figcaption>
+                                                </figure>
+                                            </div>
+                                            <div class="info-wrap">
+                                                <div class="dropdown">
+                                                    <span class="icon wb-settings" data-toggle="dropdown" aria-expanded="false" role="button"
+                                                          data-animations="fadeInDown fadeInLeft fadeInUp fadeInRight"  ></span>
+                                                    <div class="dropdown-menu dropdown-menu-right" role="menu">
+                                                        <a class="dropdown-item" href="javascript:void(0)"><i class="icon wb-pencil" aria-hidden="true"></i>Edit</a>
+                                                        <a class="dropdown-item" href="javascript:void(0)"><i class="icon wb-trash" aria-hidden="true"></i>Delete</a>
+                                                    </div>
+                                                </div> 
+                                                <div class="title">Lorem ipsum</div>
+                                                <div class="time">1 minutes ago</div>
+                                                <div class="media-item-actions btn-group" >
+                                                    <button class="btn btn-icon btn-pure btn-default" data-original-title="Edit" data-toggle="tooltip"
+                                                            data-container="body" data-placement="top" data-trigger="hover"
+                                                            type="button">
+                                                        <i class="icon wb-pencil" aria-hidden="true"></i>
+                                                    </button>
+                                                    <button class="btn btn-icon btn-pure btn-default" data-original-title="Delete"
+                                                            data-toggle="tooltip" data-container="body" data-placement="top"
+                                                            data-trigger="hover" type="button">
+                                                        <i class="icon wb-trash" aria-hidden="true"></i>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+
+                                    <li>
+                                        <div class="media-item" >
+
+                                            <div class="image-wrap"  data-toggle="slidePanel" data-url="<?php echo base_url() . "themes/default/tpl"; ?>/panel.tpl" >
+                                                <figure class="overlay overlay-hover">
+                                                    <img class="overlay-figure" src="<?php echo base_url() . "themes/default/remark"; ?>/global/photos/focus-3-960x640.jpg" alt="...">
+                                                    <figcaption class="overlay-panel overlay-background overlay-slide-right">
+                                                        <div class="img-text-hover"><h3>Title</h3>
+                                                            <p>Lorem <a href="javascript:void(0)">ipsum dolor</a> sit amet,consetetur sadipscing elitr.</p>
+                                                        </div>
+                                                    </figcaption>
+                                                </figure>
+                                            </div>
+                                            <div class="info-wrap">
+                                                <div class="dropdown">
+                                                    <span class="icon wb-settings" data-toggle="dropdown" aria-expanded="false" role="button"
+                                                          data-animations="fadeInDown fadeInLeft fadeInUp fadeInRight"  ></span>
+                                                    <div class="dropdown-menu dropdown-menu-right" role="menu">
+                                                        <a class="dropdown-item" href="javascript:void(0)"><i class="icon wb-pencil" aria-hidden="true"></i>Edit</a>
+                                                        <a class="dropdown-item" href="javascript:void(0)"><i class="icon wb-trash" aria-hidden="true"></i>Delete</a>
+                                                    </div>
+                                                </div> 
+                                                <div class="title">Lorem ipsum</div>
+                                                <div class="time">1 minutes ago</div>
+                                                <div class="media-item-actions btn-group" >
+                                                    <button class="btn btn-icon btn-pure btn-default" data-original-title="Edit" data-toggle="tooltip"
+                                                            data-container="body" data-placement="top" data-trigger="hover"
+                                                            type="button">
+                                                        <i class="icon wb-pencil" aria-hidden="true"></i>
+                                                    </button>
+                                                    <button class="btn btn-icon btn-pure btn-default" data-original-title="Delete"
+                                                            data-toggle="tooltip" data-container="body" data-placement="top"
+                                                            data-trigger="hover" type="button">
+                                                        <i class="icon wb-trash" aria-hidden="true"></i>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
@@ -111,5 +187,11 @@
     window.onload = function () {
         $('#idCompanyProjects').addClass('active');
         $('#idCompanyListProject').addClass('active');
+        $(document).on('slidePanel::afterLoad', function (e) {
+            document.getElementById("tituloNew").innerHTML = "nuevooo";
+        });
     };
+
+
+
 </script>
---
+++ themes/default/tpl/panel.tpl
@@ -0,0 +1,61 @@
+<header class="slidePanel-header overlay" style="height:350px;width:100%;background-image: url('http://localhost/smart_developer/themes/default/remark/global/photos/focus-5-960x640.jpg');"
+  data-method="scroll" data-min="100">
+    <div class="overlay-top overlay-panel overlay-background" style="padding: 0px 15px;background: rgba(0,0,0,0.5);">
+        <div class="slidePanel-actions btn-group" aria-label="actions" role="group" style="min-height: 0px;">
+      <!--<button type="button" class="btn btn-pure btn-inverse icon wb-chevron-left" aria-hidden="true"></button>
+      <button type="button" class="btn btn-pure btn-inverse icon wb-chevron-right" aria-hidden="true"></button> -->
+      <button type="button" class="btn btn-pure btn-inverse slidePanel-close icon wb-close"
+              aria-hidden="true"  style="line-height: 24px;"></button>
+    </div>
+    <h5 id='tituloNew' style="line-height: 24px;">Gardening is life.png</h5>
+  </div>
+</header>
+<div class="slidePanel-inner">
+  <section class="slidePanel-inner-section">
+    <div class="media-header">
+      <div class="tags">
+        Tags:
+        <a href="javascript: void(0)" class="badge badge-outline badge-default">Blog</a>
+        <a href="javascript: void(0)" class="badge badge-outline badge-default">Design</a>
+        <a href="javascript: void(0)" class="badge badge-outline badge-default">Cool</a>
+      </div>
+      <div class="share">
+        Share:
+        <div class="btn-group">
+          <button type="button" class="btn btn-icon btn-pure btn-default"><i class="icon bd-twitter" aria-hidden="true"></i></button>
+          <button type="button" class="btn btn-icon btn-pure btn-default"><i class="icon bd-facebook" aria-hidden="true"></i></button>
+          <button type="button" class="btn btn-icon btn-pure btn-default"><i class="icon bd-dribbble" aria-hidden="true"></i></button>
+        </div>
+      </div>
+    </div>
+    <div class="media-content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id neque quam.
+        Aliquam sollicitudin venenatis ipsum ac feugiat. Vestibulum ullamcorper
+        sodales nisi nec condimentum. Mauris convallis mauris at pellentesque volutpat.
+        Phasellus at ultricies neque, quis malesuada augue. Donec eleifend condimentum
+        nisl eu consectetur. </p>
+    </div>
+  </section>
+  <section class="slidePanel-inner-section">
+    <div class="media-header">
+      <div class="float-right">
+        <div class="time">3 minutes ago</div>
+      </div>
+      <a class="avatar mr-10" href="javascript:void(0)">
+        <img src="../../../global/portraits/2.jpg" alt="...">
+      </a>
+      <span class="name">Seevisual</span>
+    </div>
+    <div class="media-content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id neque quam.
+        Aliquam sollicitudin venenatis ipsum ac feugiat. Vestibulum ullamcorper
+        sodales nisi nec condimentum. Mauris convallis mauris at pellentesque volutpat.
+        Phasellus at ultricies neque, quis malesuada augue. Donec eleifend condimentum
+        nisl eu consectetur. </p>
+    </div>
+  </section>
+  <div class="slidePanel-comment">
+    <textarea class="maxlength-textarea form-control mb-sm mb-20" rows="4"></textarea>
+    <button class="btn btn-primary" data-dismiss="modal" type="button">Reply</button>
+  </div>
+</div>
