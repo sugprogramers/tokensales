4f52ae7a426ac85f13d1c00e715d7f2dfcb4c158
vafaster <vafaster@localhost.vafaster>
2018-08-09 18:33:02 -0500

    Fix Footer

--- application/views/footer/footer_admin.php
+++ application/views/footer/footer_admin.php
@@ -64,20 +64,6 @@
   <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/bootbox/bootbox.min599c.js?v4.0.2"></script>
   
   
-  <!-- Images -->
-  <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/jquery-ui/jquery-ui.min599c.js?v4.0.2"></script>
-  <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/blueimp-tmpl/tmpl.min599c.js?v4.0.2"></script>
-  <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/blueimp-canvas-to-blob/canvas-to-blob.min599c.js?v4.0.2"></script>
-  <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/blueimp-load-image/load-image.all.min599c.js?v4.0.2"></script>
-  <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/blueimp-file-upload/jquery.fileupload599c.js?v4.0.2"></script>
-  <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/blueimp-file-upload/jquery.fileupload-process599c.js?v4.0.2"></script>
-  <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/blueimp-file-upload/jquery.fileupload-image599c.js?v4.0.2"></script>
-  <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/blueimp-file-upload/jquery.fileupload-audio599c.js?v4.0.2"></script>
-  <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/blueimp-file-upload/jquery.fileupload-video599c.js?v4.0.2"></script>
-  <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/blueimp-file-upload/jquery.fileupload-validate599c.js?v4.0.2"></script>
-  <script src="<?php echo base_url()."themes/default/remark"; ?>/global/vendor/blueimp-file-upload/jquery.fileupload-ui599c.js?v4.0.2"></script>
-  
-  <script src="<?php echo base_url()."themes/default/remark/topbar"; ?>/assets/examples/js/forms/uploads.min599c.js?v4.0.2"></script>  
   
   
   <!-- ALERt confirmation -->
