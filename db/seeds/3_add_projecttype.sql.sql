b323dc4239b0c834f3adc44b6768f73836ab1f0f
walter <walter@sugwl01>
2018-08-09 17:35:42 -0500

    add projecttype seed

---
+++ db/seeds/3_add_projecttype.sql.sql
@@ -0,0 +1,5 @@
+INSERT INTO c_projecttype(
+            c_projecttype_id, isactive, created, createdby, updated, updatedby, 
+            name, description)
+    VALUES ('SM_PROJT01', 'Y', DATE(NOW()), '100', DATE(NOW()), '100', 
+            'Development Loan', 'A land development loan is an advance of funds, secured by a mortgage, to finance the making, installing, or constructing of the improvements necessary to convert raw land into construction-ready building sites');
