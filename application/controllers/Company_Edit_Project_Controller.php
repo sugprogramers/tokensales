93db223918fabf63f5a9ce7098365bcb55ba1294
Oscar <>
2018-08-29 11:54:51 -0500

    dashboard 1.7

--- application/controllers/Company_Edit_Project_Controller.php
+++ application/controllers/Company_Edit_Project_Controller.php
@@ -308,6 +308,7 @@ class Company_Edit_Project_Controller extends CI_Controller {
 
         $html = '<option value="">Choose a Currency</option>';
         foreach ($country_list as $country) {
+            if($country->cursymbol=='$')
             $html .= '<option value="' . $country->c_currency_id . '">' . $country->cursymbol . '</option>';
         }
         $response = array('html' => $html);
