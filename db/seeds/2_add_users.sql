061dae0b1ed667ae426a115bc0aacd4f1da98721
WINDOWS <WINDOWS@USER>
2018-09-26 11:25:00 -0500

    ADD DATABASE MODEL

--- db/seeds/2_add_users.sql
+++ db/seeds/2_add_users.sql
@@ -7,3 +7,24 @@ INSERT INTO c_user(
             'admin', '', 'SMART DEVELOPER', '', DATE(NOW()), 'Address1 #123 Street', 
             '', '100', '103', '', '', 'ADM', 
             'admin@gmail.com', '', DATE(NOW()), '');
+
+
+INSERT INTO c_user(
+            c_user_id, isactive, created, createdby, updated, updatedby, 
+            password, phone, firstname, lastname, birthday, address1, 
+            address2, c_country_id, c_region_id, city, postal, usertype, 
+            email, registertoken, tokenexpirationdate, status)
+    VALUES ('101', 'Y', DATE(NOW()), '100', DATE(NOW()), '100', 
+            '123456', '', 'SMART DEVELOPER', '', DATE(NOW()), 'Address1 #123 Street', 
+            '', '100', '103', '', '', 'INV', 
+            'investor@gmail.com', '', DATE(NOW()), '');
+
+INSERT INTO c_user(
+            c_user_id, isactive, created, createdby, updated, updatedby, 
+            password, phone, firstname, lastname, birthday, address1, 
+            address2, c_country_id, c_region_id, city, postal, usertype, 
+            email, registertoken, tokenexpirationdate, status)
+    VALUES ('102', 'Y', DATE(NOW()), '100', DATE(NOW()), '100', 
+            '123456', '', 'SMART DEVELOPER', '', DATE(NOW()), 'Address1 #123 Street', 
+            '', '100', '103', '', '', 'COMPMAN', 
+            'company@gmail.com', '', DATE(NOW()), '');
